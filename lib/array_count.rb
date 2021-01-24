61aa70372cffe753a3daac7466f854e897c9caf6
Shane R <imspammails@gmail.com>
2021-01-24 21:40:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,4 +7,9 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |element|
+    if element.class == String
+      if element == ""
+    end
+  end
 end
